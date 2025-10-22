c183836baef6580a288172199ff32f33f153b10f
Ekhsoni Olimzoda <oehsoni1@gmail.com>
2025-10-22 08:38:55 -0400

    Start creating carousel

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React from "react";
+import "./carousel.css";
 
-import amazon from '../../assets/amazon.png'
+const images = [
+  "https://picsum.photos/id/1015/400/250",
+  "https://picsum.photos/id/1016/400/250",
+  "https://picsum.photos/id/1018/400/250",
+  "https://picsum.photos/id/1020/400/250",
+  "https://picsum.photos/id/1024/400/250",
+];
 
-
-const Carousel = () => {
+function  Carousel() {
   return (
-    <div className='carousel'>
-        <img src={amazon} alt="amazon" />
-        
+    <div className="carousel">
+      <div className="carousel-track">
+        {/* We duplicate the list to make the loop seamless */}
+        {[...images, ...images].map((src, i) => (
+          <img key={i} src={src} alt={`Slide ${i}`} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Carousel
+export default Carousel;
