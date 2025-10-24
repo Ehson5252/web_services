7d2f751272344d42b42fcc57f5eeebea94c90219
Ekhsoni Olimzoda <oehsoni1@gmail.com>
2025-10-24 11:24:08 -0400

    start  About section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import './about.css';
+import '../../global.css';
+
+
+import group from '../../assets/group.jpg';
+import softwaredev from '../../assets/softwaredev.jpg';
 
 const About = () => {
   return (
     <div className="about">
+      <h2 className="title">About Us</h2>
+      <div className="main-content">
 
-        <h2>About Us</h2>
+        <div className="img-block">
+          <img src={group} alt="" />
+          <img src={softwaredev} alt="" />
+        </div>
 
+        <div className="text-block">
+         <h1 className="biggest-text">Enjoy <span>Full-Service</span> Digital Marketing Expertise</h1>
+        </div>
+
+      </div>
     </div>
     )
 }
