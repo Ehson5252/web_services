4f09ba9e7582af9e0e6a5bf6556af86d7d6e72e9
Ekhsoni Olimzoda <oehsoni1@gmail.com>
2025-11-06 22:25:41 -0500

    Worked on Services section

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import './services.css'
+
+
 import web_design_img from '../../assets/webdesign-2.jpg'
 import softwaredev from '../../assets/softwaredev.jpg';
-
+import digitalmarket from '../../assets/digitalmarket-1.jpg';
+import it_talent from '../../assets/it_consult.webp';
 
 const Services = () => {
   return (
@@ -20,23 +23,42 @@ const Services = () => {
           <img src={web_design_img} alt="" />
           <h1 className="biggest-text">WEB DESIGN</h1>
           <p>Chai Services creates responsive, user-friendly websites on platforms like WordPress, Shopify, Wix, Squarespace, and Joomla. We design visually appealing sites that engage users and bring your vision to life.</p>
-          <button className="btn">Learn More &gt; </button>
+          <div>
+            <button className="learn-more-btn">Learn More &gt; </button>
+            <button className="service-btn">Create My Website &gt; </button>
+          </div>
+
         </div>
              
         <div className="second-s grid-items">
           <h1>&lt;/Custom Software Development&gt;</h1>
-          <p>At Chai Services, we provide robust software development using Python, Java, C#, JavaScript, and PHP. Our team delivers customized, high-quality solutions, including scalable web applications and Android/iOS mobile apps, driving your business forward.</p>
-          <button className="btn">Learn More &gt; </button>
+          <p>We develop robust software using Python, Java, C#, JavaScript, React, TypeScript and PHP. Our skilled team builds high-quality, scalable web and mobile apps to drive your business forward.</p>
+          <div>
+            <button className="learn-more-btn">Learn More &gt; </button>
+            <button className="service-btn">Start My Project &gt; </button>
+          </div>
           <img src = {softwaredev} alt="" />
 
         </div>
 
         <div className="third-s grid-items">
-          <h3>Digital Marketing & SEO</h3>
+          <img src={digitalmarket} alt="" />
+          <h1>Digital Marketing & SEO</h1>
+          <p>At Chai Services, we offer digital marketing and SEO services to grow your business online. Our experts use proven strategies to boost visibility, traffic, and search rankings, helping your brand reach its full potential.</p>
+          <div>
+            <button className="learn-more-btn">Learn More &gt; </button>
+            <button className="service-btn">Grow My Business &gt; </button>
+          </div>     
         </div>
 
         <div className="fourth-s grid-items">
-          <h3>IT Talent Services</h3>
+          <h1>IT Talent Services</h1>
+          <p>We provide tailored IT services, from permanent hires and project-based consultants to full outsourcing. We also connect IT professionals with the right job opportunities for their career growth.</p>
+          <div>
+            <button className="learn-more-btn">Learn More &gt; </button>
+            <button className="service-btn">Request Consultation &gt; </button>
+          </div>
+          <img src={it_talent} alt="" />
         </div>
       </div>
 
